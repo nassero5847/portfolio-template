4d96649beaf31bfcf7bb79f4d01b338a9fc86846
nassero5847 <73483435+nassero5847@users.noreply.github.com>
2020-10-26 10:52:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let ownerName = "Your Name"
 let userName = "YourUsername";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
-    e.innerHTML = ownerName;
+    e.innerHTML = Nasser Othman;
 });
 
-document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#github").href = "http://github.com/" + nassero5847;
 document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
